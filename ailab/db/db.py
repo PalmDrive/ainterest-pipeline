ecf52f80cbbcbbc992d1e1ed99179364363e2387
ColdL <newhand.lh@gmail.com>
2017-08-02 13:41:01 +0800

    rewrite import *: no *

--- ailab/db/db.py
+++ ailab/db/db.py
@@ -2,7 +2,7 @@
 import pymysql
 import progressbar
 import time
-from ailab.config import *
+import ailab.config
 
 
 TEST_MODE = False
@@ -12,7 +12,7 @@ def medium_content_with(request_str, config=None):
     # load data from mySQL server
 
     if config is None:
-        config = config_load
+        config = ailab.config.config_load
 
     # make a connection
     try:  # if succeed
