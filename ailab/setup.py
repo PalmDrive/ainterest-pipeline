1faa78920ee813f85fd027a0d6f8356afb741f15
Junchao Wu <junchaow@uber.com>
2017-08-02 22:15:09 -0700

    remove dependency

--- ailab/setup.py
+++ ailab/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(name='ailab',
       version='0.0.1',
       description='ailab for ai gateway',
-      install_requires=['numpy', 'matplotlib', 'progressbar', 'pymysql',
+      install_requires=['numpy', 'progressbar', 'pymysql',
                         'jieba', ],
       extras_require={},
       packages=find_packages())
