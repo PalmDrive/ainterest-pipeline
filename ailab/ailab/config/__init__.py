c8c0b3d95b02eb868ec5f16fd1fe88f055c08ca2
Junchao Wu <junchaow@uber.com>
2017-08-02 22:08:53 -0700

    remove the auto load

--- ailab/ailab/config/__init__.py
+++ ailab/ailab/config/__init__.py
@@ -1,4 +1,5 @@
 import yaml
 
-with open('../../../config/config.yaml', 'r') as read_file:
-    config_load = yaml.load(read_file)
+#with open('../../../config/config.yaml', 'r') as read_file:
+#    config_load = yaml.load(read_file)
+config_load = None
