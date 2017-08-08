111fe9bc33b8c7adf486238519c677b208559e12
Junchao Wu <junchaow@uber.com>
2017-08-07 23:09:53 -0700

    fix unicode in json

--- ai_gateway/api/views.py
+++ ai_gateway/api/views.py
@@ -2,15 +2,17 @@
 from __future__ import unicode_literals
 
 import json
-
-from django.http import HttpResponse, JsonResponse
-
+import sys
+from django.http import HttpResponse
 from api import classifiers
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 def classify(request):
     data = json.loads(request.body)
     classifier = classifiers[data['type']]
     result = classifier.classify(data['content'].encode('utf-8'))
-    # result = ['ta', 'adf']
-    return JsonResponse({'status': 'ok', 'result': result})
+    return HttpResponse(
+            json.dumps({'status': 'ok', 'result': result}, ensure_ascii=False), content_type="application/json; encoding=utf-8")
